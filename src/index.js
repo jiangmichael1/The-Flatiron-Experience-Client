1d2f390961678099bf58b0a3505d4e082a2b855c
github username <github email address>
2020-05-27 22:29:34 -0400

    modified dice

--- src/index.js
+++ src/index.js
@@ -12,9 +12,29 @@ const urlHeaders= {
 }
 
 const header = document.querySelector('#header')
+const landingPageObjects = document.querySelector('#container')
+const playBtn = document.querySelector('.playBtn')
 let board = document.querySelector('#grid')
 board.style.display = "none"
-displayLandingPage()
+
+playBtn.addEventListener('click', event => {
+    landingPageObjects.remove()
+    playBtn.remove()
+    boardDisplayOnOff()
+})
+// displayLandingPage()
+
+function rollDice(){
+    let die1 = document.getElementById('die1')
+    // let die2 = document.getElementById('die2')
+    let status = document.getElementById('status')
+    let d1 = Math.floor(Math.random() * 6) + 1
+    // let d2 = Math.floor(Math.random() * 6) + 1
+    let diceTotal = d1
+    die1.innerHTML = d1
+    // die2.innerHTML = d2
+
+}
 
 function boardDisplayOnOff(){
     if (board.style.display = "none"){
@@ -24,35 +44,35 @@ function boardDisplayOnOff(){
     }
 }
 
-function displayLandingPage(){
-    const createNamePrompt = document.createElement("h2")
-    createNamePrompt.innerHTML = `
-    <h2> Enter a username: 
-        <input class="createNameTextField" type="text">
-        <button id="play" class="playBtn"> Play </button>
-    </h2>
-    `
-    header.appendChild(createNamePrompt)
-}
+// STRETCH
+//
+// function displayLandingPage(){
+//     const createNamePrompt = document.createElement("h2")
+//     createNamePrompt.innerHTML = `
+//     <h2> Enter a username: 
+//         <input class="createNameTextField" type="text">
+//         <button id="play" class="playBtn"> Play </button>
+//     </h2>
+//     `
+//     header.appendChild(createNamePrompt)
+// }
 
-const playBtn = document.querySelector('.playBtn')
-
-playBtn.addEventListener('click', event => {
-    let createUserValue = document.querySelector('.createNameTextField').value
-    // console.log(createUserValue)
-    fetch(usersUrl, {
-        method: 'POST',
-        headers: urlHeaders,
-        body: JSON.stringify({
-            name: `${createUserValue}`,
-            skills: none,
-            coding_knowledge: 0,
-            img_url: "https://www.asbmb.org/img/content-images/generic-user.jpg",
-            turns: 0
-        })
-    })
-    header.style.display = "none"
-    boardDisplayOnOff()
-})
+// playBtn.addEventListener('click', event => {
+//     let createUserValue = document.querySelector('.createNameTextField').value
+//     // console.log(createUserValue)
+//     fetch(usersUrl, {
+//         method: 'POST',
+//         headers: urlHeaders,
+//         body: JSON.stringify({
+//             name: `${createUserValue}`,
+//             skills: none,
+//             coding_knowledge: 0,
+//             img_url: "https://www.asbmb.org/img/content-images/generic-user.jpg",
+//             turns: 0
+//         })
+//     })
+//     header.style.display = "none"
+//     boardDisplayOnOff()
+// })
 
 
